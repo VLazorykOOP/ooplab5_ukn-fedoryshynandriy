e54ea5f13395e60c2b47e23540f5a74ca8e4bb00
“Andrii <andriifedoryshyn4@gmail.com>
2023-02-06 21:58:31 +0200

    first task

--- OOPLab5T/OOPLab5T.cpp
+++ OOPLab5T/OOPLab5T.cpp
@@ -1,24 +1,69 @@
-﻿// OOPLab5T.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-
+﻿using namespace std;
+#include <string>
+#include <vector>
 #include <iostream>
+class Pair
+{
+protected:
+    int x, y;
+public:
+    Pair(int x1 = 0, int y1 = 0) : x(x1), y(y1) {}
+    void SetXY(int x1, int y1)
+    {
+        x = x1;
+        y = y1;
+    }
+   
+    int Product()
+    {
+        return x * y;
+    }
+};
 
-// Ваші файли загловки 
-//
-#include "Lab5Exmaple.h"
+class Rectangle : protected Pair
+{
+ public:
+    Rectangle(int x1 = 1, int y1 = 1) : Pair(x1, y1)
+    {
+        if (x1 <= 0 || y1 <= 0) throw std::invalid_argument("arguments must be greater zero\n");
+    }
+    int Perimeter()
+    {
+        int perim = 0;
+        perim = 2 * (x + y);
+        cout << " Perimeter of rectangle  (a: " << x << ", b: " << y << ")  = ";
+        return perim;
+    }
+    int Area()
+    {   
+        int area = 0;
+        area = Product();
+        cout <<endl<< "\n Area of rectangle  (a: " << x << ", b: " << y << ")  = ";
+        return area;
+    }
+};
+
+
+int Task1()
+{
+    cout << "Task 1: \n"<<endl;
+    Rectangle pair1(3, 10);
+    cout << pair1.Perimeter();
+    cout << pair1.Area();
+    cout << endl;
+    return 0;
+}
+
 int main()
 {
-    std::cout << " Lab #5  !\n";
-    //  Код виконання завдань
-    //  Головне меню завдань
-    //  Функції та класи можуть знаходитись в інших файлах проекту
+  std::cout << "                                                Lab 5"<<endl;
+  cout << "-----------------------------------------------------------------------------------------------------" <<endl;
+  int chain = 1;
+  //int chain;
+  //cin >> chain;
 
-    int chain = 1;
-    if (chain == 1) chain = mainExample1();
-    if (chain == 2) chain = mainExample2();
-    if (chain == 3) chain = mainExample3();
-    if (chain == 4) chain = mainExample4();
-    if (chain == 5) chain = mainExample5();
-
+  if (chain == 1) chain = Task1();
+  //if (chain == 2) chain = Task2();
+  //if (chain == 3) chain = Task3();
 }
+
